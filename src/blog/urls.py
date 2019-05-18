5abc36d7c2c50b80f2c2e8ad331d61e0a4830914
albader <albaderx@gmail.com>
2019-05-18 06:51:11 +0300

    Fix bad repo

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import Home
+
 urlpatterns = [
+    path('', Home.as_view(), name='home'),
     path('admin/', admin.site.urls),
 ]
